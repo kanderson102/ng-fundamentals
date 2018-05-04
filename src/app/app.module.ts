20dddf81aa003ea33a517ec381cea858f5434de3
Kyle Anderson <kvanderson102@gmail.com>
2018-05-04 11:55:56 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { EventsListComponent } from './events-list/events-list.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsListComponent
   ],
   imports: [
     BrowserModule
